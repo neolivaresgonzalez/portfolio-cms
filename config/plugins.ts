50d9734db0db6c675309d25f0529f09f073ceb19
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-21 17:45:34 -0500

    New deploy

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        credentials: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+        },
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
