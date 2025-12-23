10a8336cf95c7652bf36cbb949e58935157c143b
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-23 09:24:23 -0500

    fix(cms): move s3 config to prod and pass aws secrets in deploy

---
+++ config/env/production/plugins.ts
@@ -0,0 +1,24 @@
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+          },
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET_NAME'),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
